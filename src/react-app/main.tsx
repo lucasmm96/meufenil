961a661cc5382be295a60f826d2a3ed1a93fd03b
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2025-12-30 19:03:15 -0300

    refactor: remove StrictMode wrapper from main rendering to simplify component structure

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@/react-app/index.css";
 import App from "@/react-app/App";
 import { AuthProvider } from "@/react-app/context/AuthContext";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </StrictMode>
+  <AuthProvider>
+    <App />
+  </AuthProvider>
 );
